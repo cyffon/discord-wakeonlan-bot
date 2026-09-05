e28688e21d2e98a6bfb568486341a83d12d272e6
tamago572 <110117095+tamago572@users.noreply.github.com>
2026-09-06 02:04:35 +0900

    wol機能を追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ChatInputCommandInteraction, Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
+import dgram from "dgram";
 
 dotenv.config();
 
@@ -16,11 +17,53 @@ interface Cmd {
   execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
 
+// ここでスラッシュコマンドを定義
 const commands: Record<string, Cmd> = {
   ping: {
     description: "ぴんぐです",
     execute: async (interaction) => {
-      await interaction.reply("ぽんぐです");
+      await interaction.reply("ぽんぐです！");
+    }
+  },
+  wol: {
+    description: "WOLパケットを送信します",
+    execute: async (interaction) => {
+      await interaction.reply(`${process.env.MAIN_PC_MAC_ADDR} にWOLパケットを送信しました`);
+
+      const formatted_macAddr = process.env.MAIN_PC_MAC_ADDR?.replace(/:/g, "").toLowerCase();
+      const macAddrBuffer = Buffer.from(formatted_macAddr!, "hex");
+
+      const wolPacket = Buffer.concat([
+        Buffer.alloc(6, 0xff),
+        Buffer.alloc(16, macAddrBuffer),
+      ])
+
+      const socket = dgram.createSocket("udp4");
+
+      socket.on("error", (err) => {
+        console.error(`WOLクライアントでエラーが発生しました: ${err}`);
+        interaction.followUp(
+          `WOLパケットの送信中にエラーが発生しました\nエラーメッセージ:\n${err}`,
+        );
+        socket.close();
+      });
+
+      socket.bind(() => {
+        socket.setBroadcast(true);
+
+        socket.send(wolPacket, 0, wolPacket.length, 9, "255.255.255.255", (err) => {
+          if (err) {
+            console.error(`WOLパケットの送信中にエラーが発生しました: ${err}`);
+            interaction.followUp(
+              `WOLパケットの送信中にエラーが発生しました\nエラーメッセージ:\n${err}`,
+            );
+          } else {
+            console.log(`WOLパケットを送信しました: ${wolPacket.toString("hex")}`);
+            interaction.followUp(`WOLパケットを送信しました: ${wolPacket.toString("hex")}`);
+          }
+          socket.close();
+        })
+      })
     }
   }
 }
@@ -31,7 +74,9 @@ client.once("ready", () => {
   const data = Object.entries(commands).map(([name, config]) => ({
     name,
     description: config.description,
-  }))
+  }));
+
+  console.log(data);
 
   client.application?.commands.set(data);
   console.log("readyです");
@@ -43,8 +88,7 @@ client.on("interactionCreate", (interaction) => {
   const command = commands[interaction.commandName];
   if (command) {
     command.execute(interaction);
-  }
-
-})
+  };
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN);
